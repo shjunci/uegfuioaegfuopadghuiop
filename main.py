4099702f8982d0edcfcda250ed325be7826673c2
Dmitrijs Sedovs <91872683+shjunci@users.noreply.github.com>
2022-01-31 09:12:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def reverseWorlds(teksts:str):
+  sarl=teksts.split()
+  if len(sarl)>1:
+    sarl.reverse()
+    teksts=""
+    for elemeats in sarl:
+      teksts+=elements
+  else:
+    teksts="Pārāk īss teikums!"
+  return teksts      
+  a = reversWorlds("")
+  print (a)
